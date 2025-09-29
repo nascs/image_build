c0051ac154bd51945f892aabc6f7ffbb08607c3b
Nascs Fang <nascs@radxa.com>
2025-09-29 10:30:10 +0800

    board_config: add qcom_addons.config
    Signed-off-by: Nascs Fang <nascs@radxa.com>

--- board_configs.sh
+++ board_configs.sh
@@ -12,7 +12,7 @@ UBOOT_DEFCONFIG=""
 
 case ${BOARD} in
 	"airbox-q900")
-		DEFCONFIG=qcom_defconfig
+		DEFCONFIG="qcom_defconfig qcom_addons.config"
 		DTB=qcs9075-radxa-airbox-q900.dtb
 		export ARCH=arm64
 		export CROSS_COMPILE=/tank3/william/toolchain/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
